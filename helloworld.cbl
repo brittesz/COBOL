2a36acf99496709472a0bc07bae1cb3123f9ffa4
Luiza <153143769+brittesz@users.noreply.github.com>
2025-04-23 21:05:56 -0300

    Add files via upload

---
+++ helloworld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:LUZIAFERq
+      * Date:2025
+      * Purpose:Study
+      * Content: My first HELLO WORLD IN COBOL
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Hello world".
+            STOP RUN.
+       END PROGRAM HELLOWORLD.
